acb59cab8438ed90ba7f7b45df60f4a66e2ccc65
Stian Lockhart Pedersen <stilo759@student.liu.se>
2022-11-10 14:48:31 +0100

    jarå

--- lab1/assignment2/assignment2.r
+++ lab1/assignment2/assignment2.r
@@ -3,16 +3,16 @@
 # test_mse: https://stackoverflow.com/questions/39811656/r-calculate-test-mse-given-a-trained-model-from-a-training-set-and-a-test-set
 # caret lecture 1d
 df = read.csv('parkinsons.csv')
-df = scale(df) ## ta bort
+
 
 set.seed(12345)
 id = sample(1:nrow(df), floor(nrow(df)*0.6))
 train_data = df[id,] ##
 test_data = df[-id,] ##
 
-#scaler = caret::preProcess(train_data)
-#train_scaled = caret::predict(scaler,train_data)
-#test_scaled = caret::predict(scaler, test_data)
+scaler = caret::preProcess(train_data)
+train_scaled = predict(scaler,train_data)
+test_scaled = predict(scaler, test_data)
 
 # y = motor_UPDRS, x = alla andra headlines?
 train_data_shaved = train_data[,-c(1,2,3,4)]
@@ -21,3 +21,14 @@ model_summary = summary(linear_model)
 model_summary
 # Calculate MSE
 mean(model_summary$residuals^2)
+
+# Loglikelihood (book 3.20)
+loglikelihood <- function(theta, dispersion)
+{
+  pi = 3.14 #pi
+  n = #number of rows in training data
+  x = train_data_shaved
+  y = # Motor_updrs row?
+  #return -(n/2) * (ln(2*pi*dispersion^2)) - (1/2*dispersion^2) * (((transpose(theta)* x) - y)^2)
+}
+
