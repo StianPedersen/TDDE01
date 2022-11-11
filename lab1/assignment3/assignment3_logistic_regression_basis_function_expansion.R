8af433378970ae99b8759e520df9a00a02b76a75
daval064 <daval064@upp-105.ad.liu.se>
2022-11-11 15:04:33 +0100

    Step 1 complete, 4 to go, VAMOS

--- lab1/assignment3/assignment3_logistic_regression_basis_function_expansion.R
+++ lab1/assignment3/assignment3_logistic_regression_basis_function_expansion.R
@@ -1,4 +1,3 @@
-Dataframe = read.csv("pima-indians-diabetes.csv", header = FALSE)
 
 #Plasma glucose as a function of age. 
 #Plasma = col V2 (Y) 
@@ -7,6 +6,24 @@ Dataframe = read.csv("pima-indians-diabetes.csv", header = FALSE)
 #main = Title
 #xlim,Ylim = 
 
-input <- Dataframe[, c('V8', 'V2')]
-diabetes
-ggplot(input, col=2)
+install.packages("tidyverse")
+library(tidyverse)
+set.seed(12345)
+
+#Q1:
+
+Dataframe = read.csv("pima-indians-diabetes.csv", header = FALSE)
+colnames(Dataframe) <- c('Tpregnant', 'PlaGluCon', 'BloodPr', 'TriSkinT', 'Insulin', 'BMI', 'DPedFunc', 'Age', 'Diabetes')
+input <- Dataframe[, c('Age', 'PlaGluCon', 'Diabetes')]
+ggplot(data = input, mapping = aes(x = Age, y = PlaGluCon, colour = Diabetes)) +
+  geom_point() + 
+  labs(title = "Plasma Glucose Concentration as a function of age") +
+  theme_bw()
+#Compact way of writing the line above
+#ggplot(input, aes(Age, PlaGluCon, colour = Diabetes)) + geom_point()
+
+#Q1 answer: No i don't think that it is easy to to classify diabetes using logistic regression
+# with only these two features. The reason being that the diabetes and non-diabetes dotes is all over the place. 
+
+#Q2:
+
