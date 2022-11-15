e63d851966cbb4370d6e93a74c1cee0feb628155
David Albrekt <daval064@student.liu.se>
2022-11-15 08:10:42 +0100

    startet on task 2

--- lab1/assignment3/assignment3_logistic_regression_basis_function_expansion.R
+++ lab1/assignment3/assignment3_logistic_regression_basis_function_expansion.R
@@ -15,6 +15,7 @@ set.seed(12345)
 Dataframe = read.csv("pima-indians-diabetes.csv", header = FALSE)
 colnames(Dataframe) <- c('Tpregnant', 'PlaGluCon', 'BloodPr', 'TriSkinT', 'Insulin', 'BMI', 'DPedFunc', 'Age', 'Diabetes')
 input <- Dataframe[, c('Age', 'PlaGluCon', 'Diabetes')]
+plot(x = input$Age, y = input$PlaGluCon, col= (input$Diabetes + 1))
 ggplot(data = input, mapping = aes(x = Age, y = PlaGluCon, colour = Diabetes)) +
   geom_point() + 
   labs(title = "Plasma Glucose Concentration as a function of age") +
@@ -25,5 +26,13 @@ ggplot(data = input, mapping = aes(x = Age, y = PlaGluCon, colour = Diabetes)) +
 #Q1 answer: No i don't think that it is easy to to classify diabetes using logistic regression
 # with only these two features. The reason being that the diabetes and non-diabetes dotes is all over the place. 
 
-#Q2:
+#Q2: glm(). lm() linnear regression
+n <- nrow(input)
+set <-input[sample(1:n,replace = FALSE), 1:3] #1:n, nrow(input), replace = TRUE, prob = c(0.8, 0.2))
+training <- set[(1:(0.8*n)),] #80 % training data.
+test <- set[((0.8*n)+1):n,] #20 % Test data
 
+m = glm(Diabetes ~ Age + PlaGluCon,
+        data = training, family = "binomial")
+
+summary(m)
