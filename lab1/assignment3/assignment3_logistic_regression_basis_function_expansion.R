5a2813f019b33da0775ea1107a3f5b6eb243e333
David Albrekt <daval064@student.liu.se>
2022-11-17 19:50:06 +0100

    assignment 3 donegit pullgit pull

--- lab1/assignment3/assignment3_logistic_regression_basis_function_expansion.R
+++ lab1/assignment3/assignment3_logistic_regression_basis_function_expansion.R
@@ -1,37 +1,33 @@
-
-#Plasma glucose as a function of age. 
-#Plasma = col V2 (Y) 
-#Age = col V8 (X) 
-#xlab, ylab = labels
-#main = Title
-#xlim,Ylim = 
-
 #install.packages("tidyverse")
 library(tidyverse)
 set.seed(12345)
 
 #Q1:
 
-Dataframe = read.csv("pima-indians-diabetes.csv", header = FALSE, na.strings = c(""))
-sapply(Dataframe, function(x) sum(is.na(x))) #Kollar om det saknas värden
-colnames(Dataframe) <- c('Tpregnant', 'PlaGluCon', 'BloodPr', 'TriSkinT', 'Insulin', 'BMI', 'DPedFunc', 'Age', 'Diabetes')
-input <- Dataframe[, c('Age', 'PlaGluCon', 'Diabetes')]
-plot(x = input$Age, y = input$PlaGluCon, col= (input$Diabetes + 1))
+Dataframe = read.csv("pima-indians-diabetes.csv", header = FALSE, na.strings = c("")) #Reads all the values to a dataframe, if a value is missing it is set to NA by na.strings
+sapply(Dataframe, function(x) sum(is.na(x))) #For each tabelcolumn checks if there is values missing
+colnames(Dataframe) <- c('Tpregnant', 'PlaGluCon', 'BloodPr', 'TriSkinT', 'Insulin', 'BMI', 'DPedFunc', 'Age', 'Diabetes') #Gives names to the columns
+input <- data.frame(Dataframe[, c('Age', 'PlaGluCon', 'Diabetes')],
+                    Z1 = (Dataframe$Age^0)*(Dataframe$PlaGluCon^4), 
+                    Z2 = (Dataframe$Age^1)*(Dataframe$PlaGluCon^3), 
+                    Z3 = (Dataframe$Age^2)*(Dataframe$PlaGluCon^2), 
+                    Z4 = (Dataframe$Age^3)*(Dataframe$PlaGluCon^1), 
+                    Z5 = (Dataframe$Age^4)*(Dataframe$PlaGluCon^0))
+#Creates a df with all the values needed in the assignment.
+
 ggplot(data = input, mapping = aes(x = Age, y = PlaGluCon, colour = Diabetes > 0.5)) +
-  geom_point() + 
-  labs(title = "Plasma Glucose Concentration as a function of age") +
-  theme_bw()
-#Compact way of writing the line above
-#ggplot(input, aes(Age, PlaGluCon, colour = Diabetes)) + geom_point()
+  geom_point() + #Creates the dotted data
+  labs(title = "Plasma Glucose Concentration as a function of age", colour = "Diabetes") + #Labels, can change x and why too i suppose
+  theme_bw() #Theme black & White
+
 
 #Q1 answer: No i don't think that it is easy to to classify diabetes using logistic regression
 # with only these two features. The reason being that the diabetes and non-diabetes dotes is all over the place. 
-
-#Q2: glm(). lm() linnear regression
+#However the PLAGLUCON AND Age can give a OK indication.
 
 #Q2.1 --> Train a logistic regression model
-n <- nrow(input)
-set <-input[sample(1:n,replace = FALSE), 1:3] #1:n, nrow(input), replace = TRUE, prob = c(0.8, 0.2))
+n <- nrow(input)#Number of rows in the input df
+set <-input[sample(1:n,replace = FALSE), 1:8] #1:n, nrow(input), replace = TRUE, prob = c(0.8, 0.2))
 training <- set[(1:(0.8*n)),] #80 % training data.
 test <- set[((0.8*n)+1):n,] #20 % Test data
 
@@ -56,8 +52,8 @@ accuracy = (confmatrix[1,1] + confmatrix[2,2])/sum(confmatrix)
 
 #Q2.3 --> Compute the training missclassification error
 #Missclassification
-missclass = (1 - sum(diag(confmatrix))/nrow(test))
-
+missclass1 = (1 - sum(diag(confmatrix))/nrow(test))
+missclass1
 dfres = as.data.frame(res)
 #test_mat = matrix(c(res, as.matrix(Age = test$Age), as.matrix(PlaGluCon = test$PlaGluCon)))
 dataPlot1 = data.frame(test,est= res > Threshold)
@@ -85,7 +81,7 @@ coef(mymodel) # = θT
 #
 
 #Q3.2 --> Add curve showing dec bound
-ggplot(data = asd, mapping = aes(x = Age, y = PlaGluCon, colour = est)) +
+ggplot(data = dataPlot1, mapping = aes(x = Age, y = PlaGluCon, colour = est)) +
   geom_point() + 
   labs(title = "Plasma Glucose Concentration as a function of age") +
   theme_bw() +
@@ -112,4 +108,30 @@ ggplot(data = dataPlot3, mapping = aes(x = Age, y = PlaGluCon, colour = est)) +
   theme_bw()
 #Q4.3 --> when r increase, less ppl are estimated to have diabetes
 
-#5.1
+#5.1 --> BASIS FFUNCTION EXPANSION. NEW FEATURES Z1-Z5
+
+#Z1 = training$PlaGluCon^4
+#Z2 = (training$Age^3)*(training$PlaGluCon^1)
+#Z3 = (training$Age^2)*(training$PlaGluCon^2)
+#Z4 = (training$Age^1)*(training$PlaGluCon^3)
+#Z5 = (training$Age^0)*(training$PlaGluCon^4)
+mymodel2 = glm(Diabetes ~ PlaGluCon + Age + Z1 + Z2 + Z3 + Z4 + Z5,
+                data = training, family = "binomial")
+summary(mymodel2)
+coef(mymodel2)
+
+res <- as.matrix(predict(mymodel2, test, type = "response"))
+Threshold = 0.5
+confmatrix <- table(Actual_Value = test$Diabetes, Predicted_Value = res > Threshold)
+confmatrix
+
+#Missclassification
+missclass = (1 - sum(diag(confmatrix))/nrow(test))  
+missclass
+
+
+dataPlot4 = data.frame(test,est= res > Threshold)
+ggplot(data = dataPlot4, mapping = aes(x = Age, y = PlaGluCon, colour = res > 0.5)) +
+  geom_point() + 
+  labs(title = "Plasma Glucose Concentration as a function of age") +
+  theme_bw()
