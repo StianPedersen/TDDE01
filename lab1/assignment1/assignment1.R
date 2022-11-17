53854c67e0fbe8fc8c7d08dd48c82b53fb3c482c
Jakob Strallhofer <jakst383@student.liu.se>
2022-11-17 20:51:27 +0100

    4/5 färdig

--- lab1/assignment1/assignment1.R
+++ lab1/assignment1/assignment1.R
@@ -1,7 +1,5 @@
 library(kknn)
 library(Formula)
-library(Misclass)
-
 
 # 1.1
 # Import the data into R and divide it into training, validation and test sets.
@@ -45,21 +43,95 @@ fit_test = fitted(m_test)
 # Confusion matrix for m_test
 table(Predicted_value=fit_test, Actual_value=test$X0.26)
 
-# Misclassification errors for test data
+# Missclassification errors for test data
 miss_class_error_test = mean(fit_test != test$X0.26)
 miss_class_error_test
-#------------------------------------------------------------------------------#
+
 # KKNN model with only train
 m_train = kknn(formula=X0.26~., train=train, test=train, k=30, kernel="rectangular")
 fit_train = fitted(m_train)
 
-miss_class_error_train = mean(fit_train != train$X0.26)
-
-
 # Confusion matrix for m_train
 table(Predicted_value=fit_train, Actual_value=train$X0.26)
 
-# Misclassification errors for training data
+# Missclassification errors for training data
 miss_class_error_train = mean(fit_train != train$X0.26)
 miss_class_error_train
+
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+
+# 1.3.
+
+p = m_train$prob
+
+index_all_eights = which(train[,65]==8)
+
+p_of_eights = p[index_all_eights,9]
+
+three_lowest_p = sort(p_of_eights)[1:3]
+print(three_lowest_p)
+m = length(p_of_eights)
+two_highest_p = sort(p_of_eights)[(m-2):m]
+
+index_two_highest_p = matrix(c(which(train[,65]==8 & p[,9]==two_highest_p)[1:2]), nrow=2, ncol = 1)
+
+index_three_lowest_p = 
+  matrix(c(which(train[,65]==8 & p[,9]==three_lowest_p[1]),
+           which(train[,65]==8 & p[,9]==three_lowest_p[2]),
+           which(train[,65]==8 & p[,9]==three_lowest_p[3])
+           ),nrow = 3,ncol = 1)
+
+# print(index_two_highest_p)
+# print(index_three_lowest_p)
+
+
+heatmaps_hard = list(
+  matrix(train[index_three_lowest_p[1],-65],nrow = 8, ncol = 8),
+  matrix(train[index_three_lowest_p[2],-65],nrow = 8, ncol = 8))
+print(heatmaps_hard[2])
+
+heatmaps_easy = list(
+  matrix(as.numeric(train[index_two_highest_p[1],-65],nrow = 8, ncol = 8)),
+  matrix(as.numeric(train[index_two_highest_p[2],-65],nrow = 8, ncol = 8)),
+  matrix(as.numeric(train[index_two_highest_p[3],-65],nrow = 8, ncol = 8)))
+print(heatmaps_easy[2])  
+
+hm_easy1 = t(matrix(as.numeric(train[index_two_highest_p[1],-65]),nrow = 8, ncol = 8))
+hm_easy2 = t(matrix(as.numeric(train[index_two_highest_p[2],-65]),nrow = 8, ncol = 8))
+
+hm_hard1 = t(matrix(as.numeric(train[index_three_lowest_p[1],-65]),nrow = 8, ncol = 8))
+hm_hard2 = t(matrix(as.numeric(train[index_three_lowest_p[2],-65]),nrow = 8, ncol = 8))
+hm_hard3 = t(matrix(as.numeric(train[index_three_lowest_p[3],-65]),nrow = 8, ncol = 8))
+
+heatmap = heatmap(hm_hard3, Colv = NA, Rowv = NA)
+
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+
+# 1.4.
+
+all_mce_train = c()
+all_mce_valid = c()
+for (i in 1:30){
+
+  m_train = kknn(formula=X0.26~., train=train, test=train, k=i, kernel="rectangular")
+  fit_train = fitted(m_train)
+  mce_train = mean(fit_train != train$X0.26)
+  all_mce_train[i] = mce_train
+  
+  m_valid = kknn(formula=X0.26~., train=train, test=valid, k=i, kernel="rectangular")
+  fit_valid = fitted(m_valid)
+  mce_valid = mean(fit_valid != valid$X0.26)
+  all_mce_valid[i] = mce_valid
+  
+  }
+
+plot(x=all_mce_train, xlab="k-value", ylim=c(0,0.06),
+     ylab="MCE", col="black", type = "b", pch=16,
+     main="Misclassification errors: Tests and Valid")
+lines(all_mce_valid, type="b", col="red", pch=16)
+legend("bottomright", legend = c("Train", "Valid"), 
+       col = c("black", "red"), pch = 16)
+help("plot")
 
