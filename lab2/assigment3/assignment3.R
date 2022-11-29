4f00397cd31b99ce2848374760aae87cf1abf13c
David Albrekt <daval064@student.liu.se>
2022-11-29 08:33:26 +0100

    Small changes to task 1

--- lab2/assigment3/assignment3.R
+++ lab2/assigment3/assignment3.R
@@ -1,2 +1,8 @@
 df <- read.csv("communities.csv", header = TRUE) #Read the data into a DF
 
+library(dplyr)
+scaler = caret::preProcess(select(df,-"ViolentCrimesPerPop")) #ViolentCrimesPerPop excluded
+scaled_df = predict(scaler, df) #scales the dataframe. (ViolentCrimesPerPop is not sclaed since the scaler dosn't include that column)
+
+PCA_matrix <- as.matrix(scaled_df)
+eigen(PCA_matrix, only.values = FALSE)
