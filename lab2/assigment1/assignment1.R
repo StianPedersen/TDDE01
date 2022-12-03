db877c1715c3d97abfc98fc192eb1d45cc484162
Jakob Strallhofer <jakst383@student.liu.se>
2022-12-03 17:08:20 +0100

    lab2 assignment 1 done

--- lab2/assigment1/assignment1.R
+++ lab2/assigment1/assignment1.R
@@ -3,9 +3,8 @@
 ###############################################################################
 
 #  ------------------------     Task 1    ----------------------------------  #
-f <- file.choose()
 # Read csv file 
-tecator = read.csv(f)
+tecator = read.csv("tecator.csv")
 tecator_modified = data.frame(tecator[,-1], row.names = tecator[,1])
 
 # Split dataframe into train and test (50/50)
@@ -13,41 +12,16 @@ n = dim(tecator_modified)[1]
 set.seed(12345)
 id = sample(1:n, floor(n*0.5))
 train = tecator_modified[id,]
-
-id1 = setdiff(1:n, id)
-set.seed(12345)
-id2 = sample(id1, floor(n*0.5))
-test = tecator_modified[id2,]
-
-train1 = train[,1:(ncol(train)-3)]
+test = tecator_modified[-id,]
 
 # Fit linear regression to the training data
-m_train=lm(train$Fat~. -1, train[,1:(ncol(train)-3)])
-
-# predict(m_train)
-summary(m_train)
-
-summary(m_train)$coefficient
-
-predict(m_train, train)
-
-ynew_lm = predict(m_train, test)
-
+m_train=lm(train$Fat~., train[,1:(ncol(train)-3)])
 
 # Estimate the training error
 mean((train$Fat - predict(m_train, train)) ^ 2)
 # Estimate the training error
 mean((test$Fat - predict(m_train, test)) ^ 2)
 
-
-# According to https://towardsdatascience.com/ridge-and-lasso-regression-a-complete-guide-with-python-scikit-learn-e20e34bcbf0b: 
-# On the other hand if we have large number of features and test score is 
-# relatively poor than the training score then it’s the problem of 
-# over-generalization or over-fitting."
-
-#  ------------------------     Task 2    ----------------------------------  #
-
-# TODO: Find 
 
 #  ------------------------     Task 3    ----------------------------------  #
 
@@ -64,7 +38,7 @@ model_lasso = glmnet(as.matrix(covariates), response, alpha = 1,
                      family="gaussian")
 
 # Produce plot of coefficients by lambda value
-plot(model_lasso, xvar="lambda")
+plot(model_lasso, xvar="lambda", xlab = expression(paste("log(", lambda, ")")), )
 
 # A summary of the glmnet path at each step is displayed if we just 
 # enter the object name or use the print function:
@@ -76,6 +50,10 @@ summary = print(model_lasso)
 lambda = summary[summary$Df==3,3]
 lambda
 
+plot(model_lasso, xvar="lambda", xlab = expression(paste("log(", lambda, ")")))
+abline(v=log(lambda[1]))
+abline(v=log(lambda[2]))
+abline(v=log(lambda[3]))
 #  ------------------------     Task 4    ----------------------------------  #
 
 # Fit Ridge regression model to training data
@@ -85,68 +63,56 @@ model_ridge = glmnet(as.matrix(covariates), response, alpha = 0,
 # Produce plot of coefficients by lambda value
 plot(model_ridge, xvar="lambda")
 
+# A summary of the glmnet path at each step is displayed if we just 
+# enter the object name or use the print function:
+summary = print(model_ridge)
+# This shows us (like the plot also did) that all coefficients are != 0 for any 
+# chosen lambda value.
+
 
 #  ------------------------     Task 5    ----------------------------------  #
 
 # Perform k-fold cross-validation to find optimal lambda value
-cv_model_lasso = cv.glmnet(as.matrix(covariates), response, alpha=1, 
+cv_model_lasso = cv.glmnet(x = covariates, y = response, alpha=1, 
                      family="gaussian")
 
 # Find optimal lambda value that minimizes test MSE
 optimal_lambda = cv_model_lasso$lambda.min
 log(optimal_lambda)
+optimal_lambda
+
 
 # Produce plot of test MSE by lambda value
-plot(x = log(cv_model_lasso$lambda), y = log(cv_model_lasso$cvm), 
-     xlab =expression(paste("log(", lambda, ")")), 
-     ylab = " Mean cross-validated error")
-abline(v=log(optimal_lambda), col="red")
+plot(cv_model_lasso)
+abline(v=log(optimal_lambda), col="orange")
 abline(v=-4, col="blue")
-legend("bottomright", legend = c(expression(paste("MSE by log(", lambda, ")")), 
+legend("bottomright", legend = c(expression(paste("MSE by og(", lambda, ")")), 
                                  expression(paste("log(", lambda, ") = - 4")), 
                                  expression(paste("Optimal log(", lambda, ")"))), 
-       col = c("black", "blue", "red"), pch = 16)
+       col = c("red", "blue", "orange"), pch = 16)
 
 
 # Create a model with the optimal lambda-value as parameter.
-optimal_model_lasso = glmnet(as.matrix(covariates), response, alpha=1, 
-                                   family="gaussian", lambda = optimal_lambda)
+optimal_model_lasso = glmnet(covariates, response, alpha=1, family="gaussian", 
+                             lambda = optimal_lambda)
 
-# We can print a summary of the model to find the number of 
-# nonzero coefficients (Df):
+# We can print a summary of the model to find the number of nonzero 
+# coefficients (Df):
 print(optimal_model_lasso)
 
-help("predict")
+# Get predictions of model with optimal lambda and linear regression model:
+ynew_lasso = predict(optimal_model_lasso, newx = as.matrix(test[,1:100]), type = "response")
+ynew_lm = predict(m_train, test)
 
+# Scatter plot of the test versus predicted test values for model corresponding
+# to optimal lambda
+plot(as.matrix(test$Fat), ynew_lasso, main = "Optimal LASSO Regression", 
+     xlab = "Original Values", ylab = "Predicted Values", 
+     ylim = c(0,max(ynew_lasso)), xlim = c(0,max(test$Fat)))
+abline(coef = c(0,1), col= "red")
 
-ynew = predict(optimal_model_lasso, newx = as.matrix(test[,1:100]), type = "response")
-
-plot(test$Fat-ynew, ylab = "Magnitutde of wrong prediction", xlab = "Channel", col = "blue")
-
-# points(test$Fat-ynew_lm, col = "red")
-abline(h=0)
-
-
-
-mean((ynew-mean(test$Fat)))
-# mean((ynew_lm-mean(test$Fat)))
-
-plot(test$Fat, col="red")
-points(ynew, col="black")
-
-sum((ynew - mean(train$Fat))^2)/sum((train$Fat - mean(train$Fat))^2)
-sum((ynew - train$Fat)^2)
-
-sum((ynew_lm - mean(train$Fat))^2)/sum((train$Fat - mean(train$Fat))^2)
-sum((ynew_lm - train$Fat)^2)
-
-
-# comparison_model_lasso = glmnet(as.matrix(covariates), response, alpha=1, 
-#                                 family="gaussian", lambda = exp(-4))
-
-
-
-# TODO: plot label vs predicted values in scatter plot 
-plot(x = train$Fat, y =  )
-
-
+# Scatter plot of the test versus predicted test values for linear regression model
+plot(as.matrix(test$Fat), ynew_lm, main = "Linear Regression",  
+     xlab = "Original Values", ylab = "Predicted Values", 
+     ylim = c(0,max(ynew_lasso)), xlim = c(0,max(test$Fat)))
+abline(coef = c(0,1), col= "red")
