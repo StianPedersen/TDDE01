7b02bb3a6bfefa2e3a364b48d3c2981b5357a2f4
Stian P <stian.lp@hotmail.no>
2022-12-07 15:26:27 +0100

    answers done?

--- lab3/assignment2/Lab3Block1_2021_SVMs_St.R
+++ lab3/assignment2/Lab3Block1_2021_SVMs_St.R
@@ -17,41 +17,59 @@ te <- spam[3801:4601, ]
 by <- 0.3
 err_va <- NULL
 for(i in seq(by,5,by)){
-  filter <- ksvm(type~.,data=tr,kernel="rbfdot",kpar=list(sigma=0.05),C=i,scaled=FALSE)
+  filter <- ksvm(type~.,data=tr,kernel="rbfdot",kpar=list(sigma=0.05),
+                 C=i,scaled=FALSE)
   mailtype <- predict(filter,va[,-58])
   t <- table(mailtype,va[,58])
   err_va <-c(err_va,(t[1,2]+t[2,1])/sum(t))
 }
 
-filter0 <- ksvm(type~.,data=tr,kernel="rbfdot",kpar=list(sigma=0.05),C=which.min(err_va)*by,scaled=FALSE)
+filter0 <- ksvm(type~.,data=tr,kernel="rbfdot",kpar=list(sigma=0.05),
+                C=which.min(err_va)*by,scaled=FALSE)
 mailtype <- predict(filter0,va[,-58])
 t <- table(mailtype,va[,58])
 err0 <- (t[1,2]+t[2,1])/sum(t)
 err0
 
-filter1 <- ksvm(type~.,data=tr,kernel="rbfdot",kpar=list(sigma=0.05),C=which.min(err_va)*by,scaled=FALSE)
+filter1 <- ksvm(type~.,data=tr,kernel="rbfdot",kpar=list(sigma=0.05),
+                C=which.min(err_va)*by,scaled=FALSE)
 mailtype <- predict(filter1,te[,-58])
 t <- table(mailtype,te[,58])
 err1 <- (t[1,2]+t[2,1])/sum(t)
 err1
 
-filter2 <- ksvm(type~.,data=trva,kernel="rbfdot",kpar=list(sigma=0.05),C=which.min(err_va)*by,scaled=FALSE)
+filter2 <- ksvm(type~.,data=trva,kernel="rbfdot",kpar=list(sigma=0.05),
+                C=which.min(err_va)*by,scaled=FALSE)
 mailtype <- predict(filter2,te[,-58])
 t <- table(mailtype,te[,58])
 err2 <- (t[1,2]+t[2,1])/sum(t)
 err2
 
-filter3 <- ksvm(type~.,data=spam,kernel="rbfdot",kpar=list(sigma=0.05),C=which.min(err_va)*by,scaled=FALSE)
+filter3 <- ksvm(type~.,data=spam,kernel="rbfdot",kpar=list(sigma=0.05),
+                C=which.min(err_va)*by,scaled=FALSE)
 mailtype <- predict(filter3,te[,-58])
 t <- table(mailtype,te[,58])
+print(t)
 err3 <- (t[1,2]+t[2,1])/sum(t)
 err3
 
 # Questions
 
-# 1. Which filter do we return to the user ? filter0, filter1, filter2 or filter3? Why?
+# 1. Which filter do we return to the user ? 
+#    filter0, filter1, filter2 or filter3? Why?
+# filter0 uses training data and predicts on validation data (800 elements), has a error of 0.0675 
+# filter1 uses training data and predicts on test data (800 elements), has a error of 0.08489
+# filter2 uses training AND validation and predicts on test data, has a error of 0.08239
+# filter3 uses the whole spam set and predicts on the test data, has a error of 0.0212
 
-# 2. What is the estimate of the generalization error of the filter returned to the user? err0, err1, err2 or err3? Why?
+# filter2 uses the highest amount of training data and predicts on a different data set
+# (on the contrary of filter3), therefore we return this filter to the user
+
+# 2. What is the estimate of the generalization error of the filter 
+#    returned to the user? err0, err1, err2 or err3? Why?
+# The filter returned to the user is filter2 and err2 is the generalization error
+# returned to the user. Generalization error is the result of the model on 
+# previously unseen data
 
 # 3. Implementation of SVM predictions.
 
@@ -59,7 +77,7 @@ sv<-alphaindex(filter3)[[1]]
 co<-coef(filter3)[[1]]
 inte<- - b(filter3)
 k<-NULL
-for(i in 1:10){ # We produce predictions for just the first 10 points in the dataset.
+for(i in 1:10){ # We produce predictions for just the first 10 points in the data set.
   k2<-NULL
   for(j in 1:length(sv)){
     k2<-0 # 0 set by me Your code here
