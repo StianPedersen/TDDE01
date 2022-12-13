ca27335903df2439123c455396c08ece03d2bb40
daval064 <daval064@student.liu.se>
2022-12-13 10:45:58 +0100

    Tsk1 done

--- lab3/assignment3/assignment3.R
+++ lab3/assignment3/assignment3.R
@@ -13,19 +13,51 @@ mydata <- data.frame(Variable, Sin=sin(Variable))
 train_data <- mydata[1:25,] # Training
 test_data <- mydata[26:500,] # Test
 # Random initialization of the weights in the interval [-1, 1]
-winit <- # Your code here
-  nn <- neuralnet(Sin ~ Variable, data = test_data) #Add more here
+set.seed(1234567890)
+winit <- unlist(cbind(c(runif(10, -1, 1)), c(runif(10, -1, 1))))# WeightInit
+  nn <- neuralnet(Sin ~ Variable,
+                  data = train_data, 
+                  hidden = c(10),
+                  startweights = winit
+                  ) #Add more here
     # Plot of the training data (black), test data (blue), and predictions (red)
-    plot(tr, cex=2)
-    points(te, col = "blue", cex=1)
-    points(te[,1],predict(nn,te), col="red", cex=1)
-
-
+    plot(train_data, cex=2)
+    points(test_data, col = "blue", cex=1)
+    points(test_data[,1],predict(nn,test_data), col="red", cex=1)
 
 ################################################################################
 #                             TASK 2                                           #
 ################################################################################
+linear <- function(x) x
+relu <- function(x) {
+  if (x < 0)
+    return(0)
+  else 
+    return (x)}
+softplus <- function(x) log(1 + exp(x))
 
+nn_linear <- neuralnet(Sin ~ Variable,
+                 data = train_data, 
+                 hidden = c(10),
+                 startweights = winit,
+                 act.fct = linear
+) #Add more here
+nn_ReLU <- neuralnet(Sin ~ Variable,
+                 data = train_data, 
+                 hidden = c(10),
+                 startweights = winit,
+                 act.fct = relu
+) #Add more here
+nn_soft <- neuralnet(Sin ~ Variable,
+                data = train_data, 
+                hidden = c(10),
+                startweights = winit,
+                act.fct = softplus
+) #Add more here
+# Plot of the training data (black), test data (blue), and predictions (red)
+plot(train_data, cex=2)
+points(test_data, col = "blue", cex=1)
+points(test_data[,1],predict(nn,test_data), col="red", cex=1)
 
 
 
