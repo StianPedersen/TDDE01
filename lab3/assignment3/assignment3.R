c9cd514e1a6ff64be36b47af1ea080a7fd68c05d
David Albrekt <daval064@student.liu.se>
2022-12-16 08:12:37 +0100

    Klart, saknas bara kommentarer

--- lab3/assignment3/assignment3.R
+++ lab3/assignment3/assignment3.R
@@ -61,24 +61,39 @@ nn_soft <- neuralnet(Sin ~ Variable,
 plot(train_data, cex=2)
 points(test_data, col = "blue", cex=1)
 points(test_data[,1],predict(nn_linear,test_data), col="red", cex=1)
-points(test_data[,1],predict(nn_ReLU,test_data), col="red", cex=1)
-points(test_data[,1],predict(nn_soft,test_data), col="red", cex=1)
+points(test_data[,1],predict(nn_ReLU,test_data), col="green", cex=1)
+points(test_data[,1],predict(nn_soft,test_data), col="yellow", cex=1)
 
 
 ################################################################################
 #                             TASK 3                                           #
 ################################################################################
-
-
-
+set.seed(1234567890)
+Variable2 <- runif(500, 0, 50)
+mydata2 <- data.frame(Variable = Variable2, Sin=sin(Variable2))
+predict(nn,mydata2)
+plot(mydata2, col = "black", cex=1)
+points(mydata2[,1], predict(nn,mydata2),  col = "blue", cex=1)
+plot(mydata2, col = "black", cex=1, ylim = c(-12,1))
+points(mydata2[,1], predict(nn,mydata2), col = "blue", cex=1)
 ################################################################################
 #                             TASK 4                                           #
 ################################################################################
 
-
-
-
+nn$weights #USED To EXPLAIN WHY IT CONVERGES TO -12
 
 ################################################################################
 #                             TASK 5                                           #
 ################################################################################
+
+set.seed(1234567890)
+Variable <- runif(500, 0, 10)
+nn_var_from_sin <- neuralnet(Variable ~ Sin,
+                data = train_data, 
+                hidden = c(10),
+                startweights = winit) #Add more here
+plot(x = train_data[,2], y = train_data[,1],
+     col = "black", ylim = c(0,10),
+     xlab = "Sinus value",
+     ylab = "X value")
+points(train_data[,2],predict(nn_var_from_sin, train_data), col = "blue")
